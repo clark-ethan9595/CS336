f4b05c0274e8376d1d380d8c2d7f441a5dc93234
clark-ethan9595 <elc3@students.calvin.edu>
2016-09-21 13:13:14 -0400

    Finished Lab3

--- Lab3/lab3.js
+++ Lab3/lab3.js
@@ -1,12 +1,23 @@
+/*
+ * lab3.js implements exercises 3.1 and 3.2 for Lab3
+ *
+ * @author: Ethan Clark (elc3)
+ * @version: fall2016
+ * CS336 Web Development
+ */
+
 var express = require('express');
 var app = express();
 
+// Send Hello World message when the url/server is called
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+// Listen on port 3000
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
+// Be able to serve up static files from the public directory
 app.use(express.static('public'));
--- Lab3/public/script.js
+++ Lab3/public/script.js
@@ -1,1 +1,2 @@
+// Script file to log a message to console for Exercise 3.3
 console.log("Lab3 - CS336: Hello, Professor Vander Linden!");
